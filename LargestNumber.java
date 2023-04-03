d2596781eff14224942e6d3846249e9fe32b794e
MILKA-WANGA <83470011+MILKA-WANGA@users.noreply.github.com>
2023-04-03 02:04:20 -0700

    Add files via upload

---
+++ LargestNumber.java
@@ -0,0 +1,25 @@
+public class LargestNumber
+{
+    public static void main(String [] args)
+    {
+        int a=5;
+        int b=7;
+        int c=10;
+
+        if(a>b && a>c)
+        {
+            System.out.println(a);
+
+        }
+        else if(b>c)
+        {
+            System.out.println(b);
+
+        }
+        else
+        {
+            System.out.println(c); 
+        }
+        
+    }
+}
